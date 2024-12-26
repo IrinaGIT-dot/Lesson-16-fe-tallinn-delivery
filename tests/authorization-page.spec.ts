0934cc3c39a71b63cad9c93a662052d170a382fe
irinabolshakova <irina.bolshakov.ee@gmail.com>
2024-12-26 18:27:57 +0200

    Updated authorization-page.spec.ts and added .env files

--- tests/authorization-page.spec.ts
+++ tests/authorization-page.spec.ts
@@ -1,26 +1,28 @@
-import { test, expect } from '@playwright/test';
-import {faker} from "@faker-js/faker/locale/en";
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker/locale/en";
 
 test.beforeEach(async ({ page }) => {
-await page.goto(process.env.APP_URL)
-  }
-)
-
-test('Check submit button is enabled', async ({ page }) => {
-  await expect(page.getByTestId('signIn-button')).toBeEnabled();
+  await page.goto(process.env.APP_URL);
 });
 
-test('Clicking submit button without login data shows pop-up', async ({ page }) => {
-  const submitButton = page.getByTestId('signIn-button')
-  await submitButton.click()
-  await expect(page.getByAltText('Please fill out this field.')).toBeVisible();
+test("Check submit button is enabled", async ({ page }) => {
+  await expect(page.getByTestId("signIn-button")).toBeEnabled();
 });
 
-test('Check authorization error with fake login and password', async ({ page }) => {
-  await page.getByTestId('username-input').fill(faker.internet.username())
-  await page.getByTestId('password-input').fill(faker.internet.password())
-  const submitButton = page.getByTestId('signIn-button')
-  await submitButton.click()
-  await expect(page.getByTestId('authorizationError-popup')).toBeVisible();
+test("Clicking submit button without login data shows pop-up", async ({
+  page,
+}) => {
+  const submitButton = page.getByTestId("signIn-button");
+  await submitButton.click();
+  await expect(page.getByAltText("Please fill out this field.")).toBeVisible();
 });
 
+test("Check authorization error with fake login and password", async ({
+  page,
+}) => {
+  await page.getByTestId("username-input").fill(faker.internet.username());
+  await page.getByTestId("password-input").fill(faker.internet.password());
+  const submitButton = page.getByTestId("signIn-button");
+  await submitButton.click();
+  await expect(page.getByTestId("authorizationError-popup")).toBeVisible();
+});
