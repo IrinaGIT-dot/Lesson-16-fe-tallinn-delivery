ebead0973d84029e383217bd4ba42fa90d0e504f
irinabolshakova <irina.bolshakov.ee@gmail.com>
2024-12-26 18:46:37 +0200

    Updated authorization-page.spec.ts

--- tests/authorization-page.spec.ts
+++ tests/authorization-page.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 import { faker } from "@faker-js/faker/locale/en";
 
+require('dotenv-url').config()
+
 test.beforeEach(async ({ page }) => {
   await page.goto(process.env.APP_URL);
 });
